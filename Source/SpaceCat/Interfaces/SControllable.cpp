5261dd15666952c92c49071c873e43574a354d07
andrescrd <c.andresricardo@gmail.com>
2020-10-17 20:29:39 -0300

    adding interface

---
+++ Source/SpaceCat/Interfaces/SControllable.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SControllable.h"
+
+// Add default functionality here for any ISControllable functions that are not pure virtual.
