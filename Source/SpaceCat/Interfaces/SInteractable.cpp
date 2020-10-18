52f178a0c744fa71fba7b4d21c36f7bf963378dd
andrescrd <c.andresricardo@gmail.com>
2020-10-18 19:50:01 -0300

    adding interactable

---
+++ Source/SpaceCat/Interfaces/SInteractable.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SInteractable.h"
+
+// Add default functionality here for any ISInteractable functions that are not pure virtual.
