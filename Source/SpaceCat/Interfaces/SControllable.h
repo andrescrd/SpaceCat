d7f0537d9b160dbf5e865d41829210ec2306910b
andrescrd <c.andresricardo@gmail.com>
2020-10-18 02:47:55 -0300

    addin bp bot with rand rotation

--- Source/SpaceCat/Interfaces/SControllable.h
+++ Source/SpaceCat/Interfaces/SControllable.h
@@ -7,7 +7,7 @@
 #include "SControllable.generated.h"
 
 // This class does not need to be modified.
-UINTERFACE(MinimalAPI)
+UINTERFACE(MinimalAPI, BlueprintType)
 class USControllable : public UInterface
 {
 	GENERATED_BODY()
