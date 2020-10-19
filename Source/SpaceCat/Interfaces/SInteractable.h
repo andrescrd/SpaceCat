6179bebbfca6891185c03114979de4ce2dc44176
andrescrd <c.andresricardo@gmail.com>
2020-10-18 22:06:49 -0300

    def activator to iinteractable

--- Source/SpaceCat/Interfaces/SInteractable.h
+++ Source/SpaceCat/Interfaces/SInteractable.h
@@ -22,16 +22,21 @@ class SPACECAT_API ISInteractable
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void SetActivatorActor(class AActor *Activator);
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	class AActor *GetActivatorActor();
 
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)	
-		void Activate(class AActor* Activator);
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void Activate(class AActor *Activator);
 
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)	
-		void Deactivate(class AActor* Activator);
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void Deactivate(class AActor *Activator);
 
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)	
-		void StartIntaraction(class AActor *CurrentActor);
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void StartIntaraction(class AActor *CurrentActor);
 
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)	
-		void StopIntaraction();
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void StopIntaraction();
 };
