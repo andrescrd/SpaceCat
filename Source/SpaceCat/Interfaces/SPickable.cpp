9bce0d2ea4ee575cebcf46d3bb22d61af1a95a53
andrescrd <c.andresricardo@gmail.com>
2020-10-21 23:19:22 -0300

    adding ipickeable

---
+++ Source/SpaceCat/Interfaces/SPickable.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SPickable.h"
+
+// Add default functionality here for any ISPickable functions that are not pure virtual.
